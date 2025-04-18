3579711f251c337a457d1df7601c6ab79d9fc666
Hadeseek <135782679+Hadeseek@users.noreply.github.com>
2025-04-18 23:35:26 +0530

    Update DBConnection.java

--- DBConnection.java
+++ DBConnection.java
@@ -4,7 +4,7 @@ public class DBConnection {
     public static Connection getConnection() throws SQLException {
         String url = "jdbc:mysql://localhost:3306/student_db";
         String user = "root";
-        String password = "yourpassword"; // Change to your actual password
+        String password = "yourpassword"; // change this
         return DriverManager.getConnection(url, user, password);
     }
 }
