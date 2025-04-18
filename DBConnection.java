bdd8390ac11ad3a511af987cb0b27dec3f28fb3b
Hadeseek <135782679+Hadeseek@users.noreply.github.com>
2025-04-18 23:26:22 +0530

    Create DBConnection.java

---
+++ DBConnection.java
@@ -0,0 +1,10 @@
+import java.sql.*;
+
+public class DBConnection {
+    public static Connection getConnection() throws SQLException {
+        String url = "jdbc:mysql://localhost:3306/student_db";
+        String user = "root";
+        String password = "yourpassword"; // Change to your actual password
+        return DriverManager.getConnection(url, user, password);
+    }
+}
