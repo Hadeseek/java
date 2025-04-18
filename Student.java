dc2142e27abce43e3b415dd47bef416f82fbe06c
Hadeseek <135782679+Hadeseek@users.noreply.github.com>
2025-04-18 23:25:53 +0530

    Create Student.java

---
+++ Student.java
@@ -0,0 +1,13 @@
+public class Student {
+    private String prn;
+    private String name;
+    private String batch;
+
+    public Student(String prn, String name, String batch) {
+        this.prn = prn;
+        this.name = name;
+        this.batch = batch;
+    }
+
+    // Getters and Setters
+}
